421d11bde8b23336e4e1524353ff7afb4590c5ab
Raul <Raul@Rauls-MacBook-Pro.local>
2016-10-20 18:55:02 -0400

    So Far

---
+++ src/main/java/com/ironyard/Logickata.java
@@ -0,0 +1,58 @@
+package com.ironyard;
+
+import java.util.List;
+
+/**
+ * Created by Raul on 10/20/16.
+ */
+public class Logickata {
+    public static int caughtSpeeding(int speed, boolean birthday)
+
+    {
+        if(birthday)
+            speed -= 5;
+        if(speed <= 60)
+            return 0;
+        else if(speed <= 80)
+            return 1;
+        else
+            return 2;
+    }
+    public static int sortaSum(int a, int b){
+
+        int sum = a + b;
+        if(sum >= 10 && sum <= 19)
+            return 20;
+        return sum;
+    }
+
+    public static boolean love6(int a, int b){
+
+        if(a == 6 || b == 6)
+            return true;
+        return ((a + b) == 6|| Math.abs(a - b) == 6);
+    }
+
+    public static String blackJack(List<Integer> dealer, List<Integer> player){
+       int x = 0;
+        int y = 0;
+
+        for (int i = 0; i < dealer.size(); i++) {
+            x = x + dealer.get(i);
+        }
+        for ( int a = 0; a < player.size(); a++){
+            y = y + player.get(a);
+        }
+          if (x>y){
+              System.out.print("Dealer");
+          }
+           if(y>x){
+            System.out.print("Player");
+        }
+        if (x==21||y==21){
+            System.out.print("BlackJack");
+        }
+
+
+    }
+}
---
+++ src/main/java/com/ironyard/Main.java
@@ -0,0 +1,131 @@
+package com.ironyard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Raul on 10/20/16.
+ */
+public class Main {
+
+    public static void main(String[] args){
+
+        testcaughtSpeedingExample();
+        testcaughtSpeedingExample2();
+        testcaughtSpeedingExample3();
+        testcaughtSpeedingExample4();
+        testsortaSumExample();
+        testsortaSumExample2();
+        testsortaSumExample3();
+        testlove6Example();
+        testlove6Example2();
+        testlove6Example3();
+        testblackJackExample();
+
+        }
+
+    private static void testcaughtSpeedingExample(){
+        int x =60;
+        boolean y = false;
+        int z = Logickata.caughtSpeeding(x, y);
+        System.out.printf("caughtSpeeding(%s, %s) -> %s\n", x,y,z);
+
+
+    }
+
+    private static void testcaughtSpeedingExample2(){
+        int x =65;
+        boolean y = false;
+        int z = Logickata.caughtSpeeding(x, y);
+        System.out.printf("caughtSpeeding(%s, %s) -> %s\n", x,y,z);
+
+
+    }
+
+    private static void testcaughtSpeedingExample3(){
+        int x =65;
+        boolean y = true;
+        int z = Logickata.caughtSpeeding(x, y);
+        System.out.printf("caughtSpeeding(%s, %s) -> %s\n", x,y,z);
+
+
+     }
+
+    private static void testcaughtSpeedingExample4(){
+        int x =81;
+        boolean y = false;
+        int z = Logickata.caughtSpeeding(x, y);
+        System.out.printf("caughtSpeeding(%s, %s) -> %s\n", x,y,z);
+
+
+    }
+    private static void testsortaSumExample(){
+        int x =3;
+        int y = 4;
+        int z = Logickata.sortaSum(x, y);
+        System.out.printf("sortaSum(%s, %s) -> %s\n", x,y,z);
+
+    }
+    private static void testsortaSumExample2(){
+        int x =9;
+        int y = 4;
+        int z = Logickata.sortaSum(x, y);
+        System.out.printf("sortaSum(%s, %s) -> %s\n", x,y,z);
+
+    }
+
+    private static void testsortaSumExample3(){
+        int x =10;
+        int y = 11;
+        int z = Logickata.sortaSum(x, y);
+        System.out.printf("sortaSum(%s, %s) -> %s\n", x,y,z);
+
+    }
+    private static void testlove6Example() {
+        int x = 6;
+        int y = 4;
+        boolean z = Logickata.love6(x, y);
+        System.out.printf("love6(%s, %s) -> %s\n", x, y, z);
+
+    }
+
+    private static void testlove6Example2() {
+        int x = 4;
+        int y = 5;
+        boolean z = Logickata.love6(x, y);
+        System.out.printf("love6(%s, %s) -> %s\n", x, y, z);
+
+    }
+
+    private static void testlove6Example3() {
+        int x = 1;
+        int y = 5;
+        boolean z = Logickata.love6(x, y);
+        System.out.printf("love6(%s, %s) -> %s\n", x, y, z);
+
+    }
+    private static void testblackJackExample(){
+        List<Integer> x = new ArrayList<Integer>();
+        x.add(1);
+        x.add(7);
+        x.add(3);
+        List<Integer> j = new ArrayList<Integer>(x);
+        List<Integer> y = new ArrayList<Integer>();
+        y.add(4);
+        y.add(10);
+        y.add(10);
+        List<Integer> a = new ArrayList<Integer>(y);
+
+       String z = Logickata.blackJack(x,y);
+
+        System.out.printf("blackJack(%s, %s) -> %s\n", x, y, z);
+
+
+
+
+    }
+
+
+    }
+
+
